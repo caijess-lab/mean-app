122be34dbedb8d17fe2ea772dbce4c769758390d
Jessica <jckjessica@hotmail.fr>
2022-06-23 00:52:12 +0200

    test fullstack activity - test routes ne fonctionne pas, test des routes une à une via postman, arrêt à l'api PUT, pas ok, le body n'est pas passé

--- backend/app.js
+++ backend/app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 // import de mongoose
 const mongoose = require('mongoose');
 // import du modèle Mongoose
-const Thing = require('./models/thing');
+const Product = require('./models/product');
 
 mongoose.connect('mongodb+srv://j_admin:45GTtid4s9GiSk@cluster0.5rv01.mongodb.net/?retryWrites=true&w=majority',
   { useNewUrlParser: true,
@@ -15,28 +15,6 @@ mongoose.connect('mongodb+srv://j_admin:45GTtid4s9GiSk@cluster0.5rv01.mongodb.ne
 // notre application
 const app = express(); // express() permet de créer l'application express
 
-// app.use(express.json());
-/* --------------------------------------- */
-/* app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
-
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-
-app.use((req, res, next) => {
-    res.json({ message : 'Votre requête a bien été reçue !'});
-    next();
-});
-
-app.use((req, res) => {
-    console.log('Réponse envoyée avec succès !');
-}); */
-
-/* -------------------------------------- */
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -46,39 +24,44 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
+app.post('/api/products', (req, res, next) => {
+    // delete req.body._id;
+    const product = new Product({
+        name: "test",
+    description: "description",
+    price: 90,
+    inStock: true
     });
-    // enregistrement de thing dans la BD
-    thing.save()
-    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    console.log(product);
+    // enregistrement du product dans la BD
+    product.save()
+    .then(() => res.status(201).json({ product: product}))
+    .catch(error => res.status(400).json({ error }));
+})
+
+app.put('/api/products/:id', (req, res, next) => {
+    console.log(req);
+    Product.updateOne({_id: req.params.id}, {...req.body, _id: req.params.id})
+    .then(() => {console.log(req.body); res.status(200).json({ id: req.params.id, message: 'Modified !'});})
     .catch(error => res.status(400).json({ error }));
 });
 
-app.put('/api/stuff/:id', (req, res, next) => {
-    Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+app.delete('/api/products/:id', (req, res, next) => {
+    Product.deleteOne({_id: req.params.id})
+    .then(() => res.status(200).json({ message: 'Deleted !'}))
     .catch(error => res.status(400).json({ error }));
 });
 
-app.delete('/api/stuff/:id', (req, res, next) => {
-    Thing.deleteOne({ _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+app.get('/api/products/:id', (req, res, next) => {
+    Product.findOne({_id: req.params.id})
+    .then(product => res.status(200).json({product: product}))
     .catch(error => res.status(400).json({ error }));
 });
 
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne({_id: req.params.id})
-    .then(thing => res.status(200).json(thing))
-    .catch(error => res.status(404).json({error}));
-});
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-    .then(things => res.status(200).json(things))
-    .catch(error => res.status(400).json({error}));
+app.get('/api/products', (req, res, next) => {
+    Product.find()
+    .then(products => res.status(200).json({products: [products]}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 /* ------------------------------------- */
